dec48ad123a4b76a418367666c1d4a107176a952
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-04-27 17:07:07 +0000

    Rename the directory - from now on it'll no longer be Relate-only, but it should grow into the OpenUAT - the open source Ubiquitous Computing Authentication Toolkit.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@298 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/dist/deploy.sh
+++
@@ -1,3 +0,0 @@
-scp dist/webstart/* sun:/srv/www/domains/mayrhofer/downloads/projects/authentication/webstart/
-scp dist/*-src.zip sun:/srv/www/domains/mayrhofer/downloads/projects/authentication/
-scp dist/*-doc.zip sun:/srv/www/domains/mayrhofer/downloads/projects/authentication/
--- src/java/RelateAuthentication/scripts/reset.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp bin:lib/relatecore.jar:. -Djava.library.path=/usr/lib uk.ac.lancs.relate.core.SerialConnector /dev/ttyUSB0
